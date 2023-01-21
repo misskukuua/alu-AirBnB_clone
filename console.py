febda1a4acfa5a98e938e5b860c880756a074d06
misskukuua <106475425+misskukuua@users.noreply.github.com>
2023-01-21 08:33:05 -0800

    Update console.py

--- console.py
+++ console.py
@@ -1,12 +1,23 @@
-"""A function to calculate the average"""
+#!/usr/bin/python3
+""" Contains command line interpreter """
 
-def calculate_average(number_list):
-    sum_list = 0
-    for number in number_list:
-        sum_list = sum_list + number
+import cmd
 
-    average = 0
-    average = sum_list / len(number_list)
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb)"
 
-   return average
-   
+    def do_EOF (self, line):
+        """ EOF Program to exit command console """
+        return True
+
+    def do_quit(self, line):
+        """ Quit program to exit the console """
+        return True
+
+    def emptyline(self):
+        """ an empty line + enter should do noting """
+        return False
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
