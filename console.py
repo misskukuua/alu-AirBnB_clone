81d4029501fe49a39ec34169e469b1580afa0c8b
misskukuua <p.asare@alustudent.com>
2023-01-21 09:34:51 -0800

    all

--- console.py
+++ console.py
@@ -24,7 +24,17 @@ class HBNBCommand(cmd.Cmd):
     creates a new class and prints its id
 """
     def do_create(self, line):
-        if 
+        if len(line) == 0:
+            print('** class name missing **')
+            return
+
+        if line[0] not in line2.keys():
+            print("** class doesn't exist **")
+            return
+
+        line3 = line2[line]()
+        line3.save()
+        print(line3.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
