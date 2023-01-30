bcd4a6f3d3bcbd17a3f36d38f8a6e826307b39fb
misskukuua <p.asare@alustudent.com>
2023-01-29 22:15:54 -0800

    all

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
 
 """ class BaseModel that defines all common attributes/methods """
+
 import uuid
 from datetime import datetime
 import models
 from uuid import uuid4
 
 
-def save(self):
-    """ Update with current time """
-    self.updated_at = datetime.today()
-    storage.save()
+class BaseModel:
+    pass
+
+    def __init__(self):
+        self.updated_at = None
+
+    def save(self, storage=None):
+        """ Update with current time """
+        self.updated_at = datetime.today()
+        storage.save()
+
+    def to_dict(self):
+        """method to create dict"""
+        first_dict = self.__dict__
 
 
-def to_dict(self):
 
-
-def __str__(self):
-
+    def __str__(self):
