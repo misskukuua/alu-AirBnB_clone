0fa631925d15cc243ce495e31ca0ca5b6b76e3e4
misskukuua <p.asare@alustudent.com>
2023-01-29 21:39:12 -0800

    PEP8 changes

---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Public class attributes """
+
+from models.base_model import BaseModel
+
+"""
+Module Amenity
+defines the Amenity class with a public attribute 'name'
+"""
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class
+    """
+    name = ""
