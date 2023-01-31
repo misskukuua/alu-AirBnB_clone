bf4a2e659cb4e37763e76320bf66ca8761f1feb0
misskukuua <p.asare@alustudent.com>
2023-01-30 23:44:34 -0800

    ne changes

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,6 @@
 import uuid
 from datetime import datetime
 import models
-from uuid import uuid4
 
 
 class BaseModel:
@@ -14,12 +13,20 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialization a new base model"""
 
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            for key, val in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    tform = "%Y-%m-%dT %H:%M:%S.%f"
+                    val = datetime.strptime(kwargs[key], tform)
+                if key != '__class__':
+                    setattr(self, key, val)
 
-    def save(self, storage=None):
+    def save(self):
         """ Update with current time"""
         self.updated_at = datetime.today()
         models.storage.save()
@@ -29,7 +36,10 @@ class BaseModel:
         Return the dictionary of the BaseModel instance.
         """
         first_dict = self.__dict__.copy()
+        first_dict["created_at"] = self.created_at.isoformat()
+        first_dict["updated_at"] = self.updated_at.isoformat()
         first_dict["__class__"] = self.__class__.__name__
+        return first_dict
 
     def __str__(self):
         """str should print class name, id, dict"""
