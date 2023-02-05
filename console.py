97e107d31b34d4a7be3339d163e10c1fd6be959c
gatwirikiriinya <f.kiriinya@alustudent.com>
2023-02-05 16:48:58 +0300

    Changed imports

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """ console interpreter """
 import cmd
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
 from models.user import User
 from models.city import City
 from models.state import State
