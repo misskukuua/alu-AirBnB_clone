f594933e96e5435365824bda6ab5af9bbae02cc0
misskukuua <p.asare@alustudent.com>
2023-02-05 14:37:06 -0800

    ne changes

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,12 +2,20 @@
 """Defines unittests for models/engine/file_storage.py."""
 
 import unittest
+from pathlib import Path
 
 import models
+from models.amenity import Amenity
+from models.city import City
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 import os
 import json
+
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class TestFileStorage(unittest.TestCase):
@@ -16,13 +24,11 @@ class TestFileStorage(unittest.TestCase):
 
     def tearDown(self):
         """Code to execute after tests are executed"""
-        # Remove file.json if it exists
         try:
             os.remove("file.json")
         except IOError:
             pass
 
-        # rename tmp.json from setUp() to file.json
         try:
             os.rename("tmp.json", "file.json")
         except IOError:
@@ -33,13 +39,34 @@ class TestFileStorage(unittest.TestCase):
     def test_all(self):
         self.assertEqual(type(self.storage.all()), dict)
 
-    def test_new(self):
-        test_model = BaseModel()
-        self.storage.new(test_model)
-        len_dict = len(self.storage.all())
-        self.assertGreater(len_dict, 0)
-
     def test_instance(self):
-        """ Check storage """
         self.assertIsInstance(models.storage, FileStorage)
 
+
+def save(self):
+    with open(self.__file_path, 'w') as outfile:
+        new_obj = {}
+        for key, value in self.__objects.items():
+            new_obj.update({key: value.to_dict()})
+        json.dump(new_obj, outfile)
+
+
+def reload(self):
+    classes = {"Amenity": Amenity,
+               "BaseModel": BaseModel,
+               "City": City,
+               "Place": Place,
+               "Review": Review,
+               "State": State,
+               "User": User}
+    my_file = Path(self.__file_path)
+    if my_file.is_file():
+        with open(self.__file_path) as json_file:
+            loads = json.load(json_file)
+            for key, value in loads.items():
+                obj = classes[value["__class__"]](**value)
+                self.__objects.update({key: obj})
+
+
+if __name__ == '__main__':
+    unittest.main()
