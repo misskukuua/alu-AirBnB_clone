39e44d540ac8e52346f7ab8a9e2affac3eedce51
misskukuua <p.asare@alustudent.com>
2023-02-05 14:11:02 -0800

    ne changes

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,45 @@
 #!/usr/bin/python3
 """Defines unittests for models/engine/file_storage.py."""
 
+import unittest
+
+import models
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 import os
 import json
-import models
+
+
+class TestFileStorage(unittest.TestCase):
+    def setUp(self):
+        self.storage = FileStorage()
+
+    def tearDown(self):
+        """Code to execute after tests are executed"""
+        # Remove file.json if it exists
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+
+        # rename tmp.json from setUp() to file.json
+        try:
+            os.rename("tmp.json", "file.json")
+        except IOError:
+            pass
+
+        FileStorage._FileStorage__objects = {}
+
+    def test_all(self):
+        self.assertEqual(type(self.storage.all()), dict)
+
+    def test_new(self):
+        test_model = BaseModel()
+        self.storage.new(test_model)
+        len_dict = len(self.storage.all())
+        self.assertGreater(len_dict, 0)
+
+    def test_instance(self):
+        """ Check storage """
+        self.assertIsInstance(models.storage, FileStorage)
+
