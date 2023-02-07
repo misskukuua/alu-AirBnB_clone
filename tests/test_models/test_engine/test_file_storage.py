576ecc8d5d98c6ac2fef584913ea195002eb2a3d
misskukuua <p.asare@alustudent.com>
2023-02-07 15:07:14 -0800

    ne changes

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,8 +2,11 @@
 import os
 import unittest
 from os import path
+
+import models
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+import json
 
 """ testing the file storage"""
 
@@ -15,10 +18,30 @@ class TestCaseFileStorage(unittest.TestCase):
         self.dir_path = 'file.json'
         self.our_model = FileStorage()
 
-    def dispose_json(self):
-        """ dispose json file """
-        if path.exists(self.dir_path):
+    def test_objects(self):
+        """Test objects is a dictionary"""
+        self.assertTrue(hasattr(FileStorage, "_FileStorage__objects"))
+        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_file_path(self):
+        self.assertTrue(hasattr(FileStorage, "_FileStorage__file_path"))
+        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
+
+    def test_instance(self):
+        """test instance."""
+        self.assertIsInstance(self.our_model, FileStorage)
+
+    def tearDown(self):
+        """Dispose json """
+        try:
             os.remove(self.dir_path)
+        except FileNotFoundError:
+            pass
+        try:
+            os.rename(self.dir_path, "tmp")
+        except FileNotFoundError:
+            pass
+        FileStorage._FileStorage__objects = {}
 
     def test_all(self):
         """ check type return by all function """
@@ -26,24 +49,31 @@ class TestCaseFileStorage(unittest.TestCase):
 
     def test_new(self):
         model = BaseModel()
-        self.our_model.new(model)
-        len_dict = len(self.our_model.all())
-        self.assertGreater(len_dict, 0)
+        models.storage.new(model)
+        self.assertNotEqual(FileStorage._FileStorage__objects, {})
+        obj = (model.__class__.__name__ + '.' +
+               model.id)
+        self.assertIn(obj, FileStorage._FileStorage__objects.keys())
+
+    # def test_new(self):
+    #     model = BaseModel()
+    #     self.our_model.new(model)
+    #     len_dict = len(self.our_model.all())
+    #     self.assertGreater(len_dict, 0)
 
     def test_save(self):
         self.our_model.save()
         self.assertEqual(path.exists(self.dir_path), True)
 
-    def test_reload(self):
-        model = FileStorage()
-        self.our_model.reload()
-        len_dict = len(model.all())
-        self.assertGreater(len_dict, 0)
+    def test_reload_no_file(self):
+        self.assertRaises(FileNotFoundError, models.storage.reload())
 
-    def tearDown(self):
-        """ destroys created file """
-        FileStorage._FileStorage__file_path = "file.json"
-        try:
-            os.remove("test.json")
-        except FileNotFoundError:
-            pass
+    def test_reload_with_arg(self):
+        with self.assertRaises(TypeError):
+            models.storage.reload(None)
+
+    # def test_reload(self):
+    #     model = FileStorage()
+    #     self.our_model.reload()
+    #     len_dict = len(model.all())
+    #     self.assertGreater(len_dict, 0)
