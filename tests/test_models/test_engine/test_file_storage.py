4358aa34786c1cff9f6817d570241d1a842a3587
misskukuua <p.asare@alustudent.com>
2023-02-07 01:22:18 -0800

    ne changes

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,46 +1,71 @@
 #!/usr/bin/python3
-import os
+""" Unittests FileStorage Module"""
+
 import unittest
-from os import path
-from models.base_model import BaseModel
+
+import pep8
+
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+import json
+import os
+import uuid
 
-""" testing the file storage"""
+
+class TestFileStorage(unittest.TestCase):
+    """ Test FileStorage obj Methods """
+
+    def test_assert_stylepep8_amenity(self):
+        """ Test for style model """
+        style = pep8.StyleGuide(quiet=True)
+        new = style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(new.total_errors, 0, "Error pep8 base model")
+
+    def test_assert_stylepep8_testsamenity(self):
+        """ Test for style tests """
+        s = pep8.StyleGuide(quiet=True)
+        n = s.check_files(
+            ['tests/test_models/test_engine/test_file_storage.py'])
+        self.assertEqual(n.total_errors, 0, "Error pep8 tests")
+
+    def test_assert_docstring(self):
+        """ Test docstring """
+        self.assertTrue(len(FileStorage.__doc__) > 1)
+        self.assertTrue(len(FileStorage.all.__doc__) > 1)
+        self.assertTrue(len(FileStorage.new.__doc__) > 1)
+        self.assertTrue(len(FileStorage.save.__doc__) > 1)
+        self.assertTrue(len(FileStorage.reload.__doc__) > 1)
+
+    def test_assert_is_instance(self):
+        """ Test init instance is ok """
+        a = FileStorage()
+        self.assertIsInstance(a, FileStorage)
+
+    def test_assert_is_subclass(self):
+        """ Test FileStorage is subclss BaseM """
+        a = FileStorage()
+        self.assertTrue(issubclass(self.a.__class__, BaseModel), True)
+
+    def test_assert_args(self):
+        """Test User have args"""
+        a = FileStorage(8)
+        self.assertEqual(type(a).__name__, "FileStorage")
+        self.assertFalse(hasattr(a, "8"))
+
+    def test_assert_save(self):
+        """Test save method"""
+        a = BaseModel()
+        self.assertIsInstance(a, BaseModel)
+        self.storage.new(a)
+        self.storage.save()
+        self.assertTrue(self.storage._FileStorage__file_path)
+        self.assertTrue(self.storage._FileStorage__objects)
+        self.assertTrue(os.path.isfile(self.storage._FileStorage__file_path))
+        key = "BaseModel.{}".format(a.id)
+        self.storage._FileStorage__objects = {}
+        self.storage.reload()
+        self.assertDictEqual(self.storage.all()[key].to_dict(), a.to_dict())
 
 
-class TestCaseFileStorage(unittest.TestCase):
-    """ class for test cases """
-
-    def set_up(self):
-        self.dir_path = 'file.json'
-        self.our_model = FileStorage()
-
-    def dispose_json(self):
-        """ dispose json file """
-        if path.exists(self.dir_path):
-            os.remove(self.dir_path)
-
-    def test_all(self):
-        """ check type return by all function """
-        self.assertEqual(type(self.our_model.all()), dict)
-
-    def test_new(self):
-        model = BaseModel()
-        self.our_model.new(model)
-        len_dict = len(self.our_model.all())
-        self.assertGreater(len_dict, 0)
-
-    def test_save(self):
-        self.our_model.save()
-        self.assertEqual(path.exists(self.dir_path), True)
-
-    # def test_reload(self):
-    #     model = FileStorage()
-    #     self.our_model.reload()
-    #     len_dict = len(model.all())
-    #     self.assertGreater(len_dict, 0)
-
-    def test_reload(self):
-        """test reload"""
-        self.our_model.reload()
-        self.assertIsInstance(self.our_model.all(), type({}))
+if __name__ == "__main__":
+    unittest.main()
