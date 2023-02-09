7bab189d1f5917ed2fcd2ff9ab231c6ce130e330
misskukuua <p.asare@alustudent.com>
2023-02-09 12:39:09 -0800

    ne changes

--- models/base_model.py
+++ models/base_model.py
@@ -13,22 +13,27 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialization a new base model"""
 
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
-        else:
+        # form = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if len(kwargs) != 0:
             for key, val in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    tform = "%Y-%m-%dT %H:%M:%S.%f"
-                    val = datetime.strptime(kwargs[key], tform)
+                if key == "self.created_at" or key == "self.updated_at":
+                    val = datetime.fromisoformat(val)
+                    setattr(self, key, val)
+                    continue
                 if key != '__class__':
                     setattr(self, key, val)
+                else:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = datetime.utcnow()
+                    self.updated_at = datetime.utcnow()
+                    models.storage.new(self)
 
     def save(self):
         """ Update with current time"""
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
