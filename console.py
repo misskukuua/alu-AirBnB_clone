51e2675a54faa5552fc07d4f12b540f766ef9210
gatwirikiriinya <f.kiriinya@alustudent.com>
2023-02-10 19:06:30 +0300

    modified console

--- console.py
+++ console.py
@@ -40,111 +40,101 @@ class HBNBCommand(cmd.Cmd):
         """ an empty line + enter should do nothing """
         return False
 
-    def do_create(self, args):
+    def do_create(self, line):
+        """ creates a BaseModel instance into JSON file-creates a
+         new class and prints its id
         """
-            Creates a new instance of a class,
-            saves it (to the JSON file) and prints the id.
-            Usage: create <class_name>
+        if not line:
+            print("** class name missing **")
+        else:
+            if line not in self.__classes:
+                print("** class doesn't exist **")
+            else:
+                line = eval(line + "()")
+                line.save()
+                print(line.__dict__['id'])
+
+    def do_show(self, line):
+        """Prints the string representation of an instance
+          based on the class name and id
        """
-        
-        if len(args) < 1:
+        show = line.split()
+        objdict = models.storage.all()
+
+        if len(show) == 0:
+            """print class name is name is missing If
+             the class name is missing,
+            """
             print("** class name missing **")
             return
-       
-        args = args.split()
 
-        class_name = args[0]
-        if class_name not in self.__classes:
+        elif len(show) == 1:
+            """If the id is missing, print ** instance id missing **"""
+            print("** instance id missing **")
+            return
+
+        elif show[0] not in HBNBCommand.__classes:
+            """If the class name doesn’t exist,
+             print ** class doesn't exist **
+            """
             print("** class doesn't exist **")
             return
-        
-        new_object = eval(class_name + "()")
-        new_object.save()
-        print(new_object.id)
-        storage.save()
-        
 
-    def do_show(self, args):
-        """Usage: to show <class> <id> or <class>.show(<id>)
-        Display string representation of a class instance of given id.
+        elif "{}.{}".format(show[0], show[1]) not in objdict:
+            print("** no instance found **")
+            return
+        else:
+            print(objdict["{}.{}".format(show[0], show[1])])
+
+    def do_destroy(self, line):
         """
-        arg_list = args.split()
-        objdict = storage.all()
-        if len(arg_list) == 0:
+       Deletes an instance based on the class name and id
+       Save changes into a JSON file
+       """
+        destroy = line.split()
+        des_objdict = models.storage.all()
+        if len(destroy) == 0:
             print("** class name missing **")
             return
-        elif arg_list[0] not in self.__classes:
-            print("** class doesn't exist **")
-            return
-        elif len(arg_list) == 1:
+
+        elif len(destroy) == 1:
             print("** instance id missing **")
             return
-
-        object_key = "{}.{}".format(arg_list[0], arg_list[1])
-
-        if object_key not in objdict:
-            print("** no instance found **")
-            return
         else:
-            print(objdict[object_key])
+            key = "{}.{}".format(destroy[0], destroy[1])
+            if key not in des_objdict.keys():
+                print("** no instance found **")
+            else:
+                del des_objdict[key]
+                models.storage.save()
 
-
-    def do_destroy(self, args):
-        """Usage: to destroy <class> <id> or <class>.destroy(<id>)
-        Delete class instance of given id."""
-
-        arg_list = args.split()
-        all_objects = storage.all()
-        if len(arg_list) == 0:
-            print("** class name missing **")
-            return
-        class_name = arg_list[0]
-        if class_name not in self.__classes:
-            print("** class doesn't exist **")
-            return
-        if len(arg_list) == 1:
-            print("** instance id missing **")
-            return
-        instance_id = arg_list[1]
-
-        object_key = "{}.{}".format(class_name, instance_id)
-
-        if object_key not in all_objects.keys():
-            print("** no instance found **")
-        else:
-            del all_objects[object_key]
-            storage.save()
-
-    def do_all(self, args):
+    def do_all(self, line):
         """
        Prints all string representation of all instances
        based or not on the class name.
        """
-        arg_list = args.split()
-        all_objects = storage.all()
-        object_list = []
-        if len(arg_list) == 0:
-            for obj in all_objects.values():
-                object_list.append(obj.__str__())
-            print(list(object_list))
-            return
-
-        if len(arg_list) > 0 and arg_list[0] not in self.__classes:
+        dall = line.split()
+        if len(dall) > 0 and dall[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
-            return
-        class_name = arg_list[0]
-        object_list = []
 
-        for obj in all_objects:
-            if class_name == all_objects[obj].__class__.__name__:
-                object_list.append(str(all_objects[obj]))
-        print(object_list)
+        else:
+            obj1 = models.storage.all()
+            objects = obj1.values()
+            obj2 = []
+            for obj in objects:
+                if len(dall) > 0 and dall[0] == obj.__class__.__name__:
+                    obj2.append(obj.__str__())
+                elif len(dall) == 0:
+                    obj2.append(obj.__str__())
+            print(obj2)
 
     def do_update(self, line):
         """
-       updates an instance based on the class name and id by adding or updating attribute
+       updates an instance based on the class name and id
+       by adding or updating attribute
        save the change into the JSON file
-       Usage: update <class name> <id> <attribute name> "<attribute value>"
+       Usage: update <class name> <id>
+       <attribute name> "<attribute value>"
        """
         update = line.split()
         objdict = storage.all()
