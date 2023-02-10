de807c0bcf3d9f67bccd89a5dc4bcb1654813a6b
gatwirikiriinya <f.kiriinya@alustudent.com>
2023-02-10 21:21:54 +0300

    PEP8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,7 @@
 """
 store by serialization and deserialization 
 """
-
+import os.path
 import json
 
 
@@ -15,22 +15,23 @@ class FileStorage:
 
     def all(self):
         """returns dict _objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """
         sets in objects the obj with key 
         """
-        key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        key = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes objects to a Json file"""
-        object_dict = {}
-        for obj in self.__objects:
-            object_dict[obj] = self.__objects[obj].to_dict()
-        with open(self.__file_path, 'w') as file:
-            json.dump(object_dict, file)
+        dictionary = {}
+
+        for key, value in FileStorage.__objects.items():
+            dictionary[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+            json.dump(dictionary, f)
 
     def reload(self):
         """deserializes the Json file  back to objects"""
@@ -43,11 +44,11 @@ class FileStorage:
         from models.amenity import Amenity
         from models.review import Review
 
-        try:
-            with open(self.__file_path) as file:
-                serialized_content = json.load(file)
-                for item in serialized_content.values():
-                    class_name = item['__class__']
-                    self.new(eval(class_name + "(**" + str(item) + ")"))
-        except FileNotFoundError:
-            pass
+        lst = {'BaseModel': BaseModel, 'User': User,
+               'Place': Place, 'City': City, 'Amenity': Amenity,
+               'State': State, 'Review': Review}
+
+        if os.path.exists(FileStorage.__file_path) is True:
+            with open(FileStorage.__file_path, 'r') as f:
+                for key, value in json.load(f).items():
+                    self.new(lst[value['__class__']](**value))
